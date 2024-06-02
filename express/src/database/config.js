f002f312cf89d4187b62edaebd18d5cf7209ab60
Tanisha <s3975655@student.rmit.edu.au>
2024-06-02 21:08:24 +1000

    config.js and cart export

---
+++ express/src/database/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HOST: "rmit.australiaeast.cloudapp.azure.com",
+    USER: "s3980305_fsd_a2",
+    PASSWORD: "Virat_18",
+    DB: "s3980305_fsd_a2",
+    DIALECT: "mysql"
+  };
+  
---
+++ express/src/database/models/cart.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define("cart", {
+      cart_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+        unique: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      tableName: "carts",
+      timestamps: true
+    });
+  
+    return Cart;
+  };
+  
